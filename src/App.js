2874a0a13937d2db304b846760d879af08c37ab1
Akshayak8 <srigadaakshay@gmail.com>
2024-12-08 15:08:50 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, Link } from "react-router-dom";
 import About from "./components/About";
 import Skills from "./components/Skills";
 import Experience from "./components/Experience";
