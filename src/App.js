bf53ce19a58b3f874833b3d8ed560553853a1dad
Akshayak8 <srigadaakshay@gmail.com>
2025-01-19 15:52:29 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React, { useState } from "react";
 import "./App.css";
-import { HashRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import About from "./components/About";
 import Skills from "./components/Skills";
 import Experience from "./components/Experience";
 import Projects from "./components/Projects";
 import Achievements from "./components/Achievements";
-import ScrollToTop from "./components/ScrollToTop";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faPen,
@@ -16,19 +15,17 @@ import {
   faGraduationCap,
   faUserTie,
 } from "@fortawesome/free-solid-svg-icons";
-import Blog from "./components/Blog";
 
 function App() {
   const [menuOpen, setMenuOpen] = useState(false);
 
   const handleLinkClick = () => {
     setMenuOpen(false);
+    window.scrollTo(0, 0);
   };
 
   return (
     <Router>
-      <ScrollToTop />
-      {/* Overlay content */}
       <div className="content">
         <header className="header">
           <nav className="nav">
@@ -41,7 +38,7 @@ function App() {
               <div className={`menu1 ${menuOpen ? "open" : ""}`}>
                 <div className="home" onClick={handleLinkClick}>
                   <FontAwesomeIcon icon={faPen} />
-                  <Link to="/Blog">Blogs</Link>
+                  <a href="https://guideboard-mu.vercel.app/">Blogs</a>
                 </div>
                 <div className="home" onClick={handleLinkClick}>
                   <FontAwesomeIcon icon={faLightbulb} />
@@ -71,7 +68,6 @@ function App() {
         <main className="main-content">
           <Routes>
             <Route path="/" element={<About />} />
-            <Route path="/Blog" element={<Blog />} />
             <Route path="/Skills" element={<Skills />} />
             <Route path="/Experience" element={<Experience />} />
             <Route path="/Projects" element={<Projects />} />
@@ -82,18 +78,18 @@ function App() {
         {/* Bottom navigation links */}
         <div className="bottom-box">
           <div className="hom">
-            <Link to="/Blog">Blogs</Link>
+            <a href="https://guideboard-mu.vercel.app/">Blogs</a>
           </div>
-          <div className="hom">
+          <div className="hom" onClick={handleLinkClick}>
             <Link to="/Skills">Skills</Link>
           </div>
-          <div className="hom">
+          <div className="hom" onClick={handleLinkClick}>
             <Link to="/Experience">Experience</Link>
           </div>
-          <div className="hom">
+          <div className="hom" onClick={handleLinkClick}>
             <Link to="/Projects">Projects</Link>
           </div>
-          <div className="hom">
+          <div className="hom" onClick={handleLinkClick}>
             <Link to="/Achievements">Achievements</Link>
           </div>
         </div>
